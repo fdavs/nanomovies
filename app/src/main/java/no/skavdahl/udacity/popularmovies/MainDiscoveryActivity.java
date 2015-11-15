3b45c03c2634135d49347e18853f1c2a2a75d649
Frank Davidsen <frank.davidsen@outlook.com>
2015-11-15 14:03:44 +0100

    Moves the Refresh settings from MainDiscoveryActivity to MainDiscoveryActivityFragment.

--- app/src/main/java/no/skavdahl/udacity/popularmovies/MainDiscoveryActivity.java
+++ app/src/main/java/no/skavdahl/udacity/popularmovies/MainDiscoveryActivity.java
@@ -1,7 +1,6 @@
 package no.skavdahl.udacity.popularmovies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -28,26 +27,4 @@ public class MainDiscoveryActivity extends AppCompatActivity {
         //});
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_discovery, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_refresh) {
-            // TODO Query the movie DB
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/no/skavdahl/udacity/popularmovies/MainDiscoveryActivityFragment.java
+++ app/src/main/java/no/skavdahl/udacity/popularmovies/MainDiscoveryActivityFragment.java
@@ -3,6 +3,9 @@ package no.skavdahl.udacity.popularmovies;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -17,6 +20,33 @@ public class MainDiscoveryActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_main_discovery, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main_discovery, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refreshMovies();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Re-issues the current query to themoviedb.org and updates the display with the
+     * results from the reissued query.
+     */
+    private void refreshMovies() {
+
     }
 }
